822c6d63b76ee4f77b0774e702998fba0e1c59fa
gbohnen <fezmonkey@live.com>
2016-04-01 11:21:54 -0600

    Initial Scene Work

---
+++ Assets/Calibration.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Calibration : MonoBehaviour {
+
+    public float metronomeInterval;
+    public float calibrationMax;
+
+
+
+
+    public GameObject circle;
+    public GameObject timerField;
+    public GameObject intervalField;
+
+    public InputField timer;
+    public InputField interval;
+
+    public float calibrationTimer;
+    public float intervalTimer;
+    public int mode;
+
+    public void Start()
+    {
+        timer = timerField.GetComponent<InputField>();
+        interval = timerField.GetComponent<InputField>();
+
+        interval.onValueChanged.AddListener(delegate { SetInterval(); });
+    }
+
+
+    public void SetTimer()
+    {
+
+    }
+
+    public void SetInterval()
+    {
+        Debug.Log(timer.text);
+    }
+
+    public void StartTimer()
+    {
+
+    }
+
+
+
+}
---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+	public void HomeButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void CalibrationButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void TestingButton()
+    {
+
+    }
+}
